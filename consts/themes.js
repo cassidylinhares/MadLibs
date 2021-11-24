bb4d3e5aba4e38dacdf71781d579b97ccca2545f
Shaeyi-lu <yemisicoast@gmail.com>
2021-11-24 18:28:06 -0500

    starting home page

---
+++ consts/themes.js
@@ -0,0 +1,23 @@
+const themes = [
+    {
+        id: 1,
+        name: 'christmas',
+        img: require('')
+    },
+    {
+        id: 2,
+        name: 'halloween',
+        img: require('')
+    },
+    {
+        id: 3,
+        name: 'mean girls',
+        img: require('')
+    },
+
+    {
+        id:4,
+        name: 'criminal',
+        img: require('')
+    }
+]
