d466fd22254a27a8d7a257215b02d76b2fa35aea
Shaeyi-lu <yemisicoast@gmail.com>
2021-11-26 15:06:31 -0500

    colour adjustments

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,12 +5,21 @@ const width = Dimensions.get("screen").width/2-30
 
 const Home = ({navigation}) => {
 
-    const themes = [{id:1, name: 'christmas',img: require("../assets/images/xmas.jpg"), about: "We're making it up as we go along. Here's a Christmas story for you!"}, {id:2, name:'meangirls',img: require("../assets/images/meangirls.jpg"), about: "Get in loser, we're playing madlibs. We're making it up as we go along. Here's a story from the all time classic for you!"}, {id:3, name:'fish', img: require("../assets/images/halloween.jpg"),about: "We're making it up as we go along. Here's a Christmas story for you!"}, {id:4, name:'fire', img: require("../assets/images/mystery.jpg"),about: "We're making it up as we go along. Here's a Christmas story for you!"}, {id:5, name:'flower', img: require("../assets/images/mystery.jpg"), about: "We're making it up as we go along. Here's a Christmas story for you!"}, {id:6, name:'sky', img: require("../assets/images/spiderman.jpg"),about: "We're making it up as we go along. Here's a Christmas story for you!"},];
+    //array for the cards with respective images and about info (tease)
+    const themes = [
+        {id:1, name:'christmas',img: require("../assets/images/xmas.jpg"), about: "It's looking a lot like Christmas. We're making it up as we go along, here's a Christmas story for you!"}, 
+        {id:2, name:'meangirls',img: require("../assets/images/meangirls.jpg"), about: "Get in loser, we're playing madlibs. We're making it up as we go along, here's a story from the all time classic for you!"},
+        {id:3, name:'halloween', img: require("../assets/images/halloween.jpg"),about: "OoooH, spooooky. We're making it up as we go along, here's a Halloween story for you!"}, 
+        {id:4, name:'mystery1', img: require("../assets/images/mystery.jpg"),about: "Hmm, secretive. You gotta play to find out cause we're making it up as we go along. Here's a mystery story for you!"}, 
+        {id:5, name:'mystery2', img: require("../assets/images/mystery.jpg"), about: "Hmm, secretive. You gotta play to find out cause we're making it up as we go along. Here's a mystery story for you!"}, 
+        {id:6, name:'spiderman', img: require("../assets/images/spiderman.jpg"),about: "We're making it up as we go along. Here's a Christmas story for you!"},];
 
-    const categories=['ALL', 'MOVIES', 'HOLIDAYS', 'RANDOM'];
+    const categories=['ALL', 'MOVIES', 'HOLIDAYS', 'RANDOM']; //array holding the category titles
 
     const [categoryIndex, setCateoryIndex] = React.useState(0);
 
+
+    //function to display the categories and colour change to the title when a category is selected
     const CategoryList = () => {
         return (
             <View style={styles.categoryContainer}>
@@ -31,6 +40,7 @@ const Home = ({navigation}) => {
         );
     };
 
+    //Card function to return image with respect to the array 'themes'
    const Card = ({theme}) => {
         return (
         <TouchableOpacity onPress={() => navigation.navigate("Play", theme)}>
@@ -44,33 +54,40 @@ const Home = ({navigation}) => {
     };
 
     return (
-        <SafeAreaView style={{flex:1, paddingHorizontal:20, backgroundColor: "white",}}>
-            <View style={styles.header}>
+        <SafeAreaView style={{flex:1, backgroundColor: "white",}}>
+            <View style={styles.header}> 
                 
               <View>
-                <Text style={{fontSize: 30, fontWeight: 'bold'}}>MAD LIBS</Text>
+                <Text style={{fontSize: 30, fontWeight: 'bold', color: 'black'}}>MAD LIBS</Text>
               </View>
-              <Icon style={{marginTop: 7,}} name="bars" size={30} color="#CBC3E3"/>
+              <Icon style={{marginTop: 7,}} name="bars" size={30} color="#5D3FD3"/>
             </View>
            
-            <CategoryList/>
+           {/*Calling the CategoryList function as a tag */}
+            <CategoryList/> 
+            
+             {/*Flatlist is a 'table' like tag, to make the layout of card display in 2 columns */}
             <FlatList  
                 columnWrapperStyle={{justifyContent: 'space-between'}}
                 showsVerticalScrollIndicator= {false}
                 contentContainerStyle={{
-                    marginTop: 10,
+                    paddingTop:20,
                     paddingBottom: 50,
+                    paddingHorizontal:15, 
+                    backgroundColor: "#5D3FD3"
                 }}
                 numColumns={2}
                 data={themes}
                 renderItem={({item}) => <Card theme={item}/>}
             />
+          
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     header: {
+        paddingHorizontal:20, 
         marginTop: 50,
         flexDirection: 'row',
         justifyContent: "space-between",
@@ -87,9 +104,10 @@ const styles = StyleSheet.create({
         
     },
     categoryContainer:{
+        paddingHorizontal:20, 
         flexDirection: 'row',
         marginTop: 30,
-        marginBottom: 20,
+        marginBottom: 15,
         justifyContent: 'space-between',
     },
     categoryText: {
@@ -98,7 +116,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     categoryTextSelected: {
-        color: "#CBC3E3",
+        color: "#5D3FD3",
         paddingBottom: 5,
         borderBottomWidth: 2,
         borderColor: "#CBC3E3"
