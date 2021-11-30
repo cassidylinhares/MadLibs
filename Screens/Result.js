b061cb9bd830f7c9152a2abb3512ad3d3765c3ad
mayarshaath <57917588+mayarshaath@users.noreply.github.com>
2021-12-01 02:05:51 +0300

    added result screen

---
+++ Screens/Result.js
@@ -0,0 +1,40 @@
+
+
+const styles = StyleSheet.create({
+    header: {
+        paddingHorizontal: 20,
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    title: {
+        flex: 0.15,
+        marginTop: 5,
+        color: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    detailsContainer: {
+        flex: 1,
+        height: 100,
+        backgroundColor: "#F8F8F8",
+        marginHorizontal: 15,
+        borderRadius: 20,
+        marginTop: 30,
+        paddingTop: 30
+    },
+    btn: {
+        marginTop: 50,
+        width: 290,
+        marginHorizontal: 20,
+        height: 70,
+        backgroundColor: '#5D3FD3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30,
+
+    }
+});
+
+export default Result;
