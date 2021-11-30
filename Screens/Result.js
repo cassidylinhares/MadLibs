d21326235a652303f69908547eb45a04294e718b
mayarshaath <57917588+mayarshaath@users.noreply.github.com>
2021-12-01 02:11:50 +0300

    update

--- Screens/Result.js
+++ Screens/Result.js
@@ -1,4 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { getStory } from '../firebase';
 
+const Result = ({navigation, route}) => {
+    //get parameters passed from the play screen
+    const theme = route.params;
+
+    const [story, setStory] = useState({});
+    const [blanks, setBlank] = useState([]);
+    
+    //on initializing the component
+    useEffect(() => {
+        async function fetchStory() {
+            const storyFetched = await getStory(theme.id);
+            setStory({...storyFetched});
+            setBlank([...storyFetched.blanks]);
+        } 
+        fetchStory();    
+    }, []);
+
+    const handlePlayAgainButton = () => {
+        navigation.navigate("Home", theme);
+    }
+
+    //makes the ui for the blank word types
+    const mapBlanks = () => {
+        //handler when user changes the text
+        const handleBlankInput = (text, i) => {
+            let blanksTemp = blanks; //copy blanks
+            blanksTemp[i] = text; //alter the copy
+            setBlank(blanksTemp); //change it to the altered copy
+        }
+        //the ui
+        return story?.blanks?.map((blank, index) => (
+            <View style={styles.title} key={index}>
+                <TextInput onChangeText={text => handleBlankInput(text, index)}/>
+                <Text>{blank}</Text>
+            </View>
+        ));
+    }
+
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: "#CBC3E3"}}>
+        <View style={styles.detailsContainer}>
+            <View style={styles.title}>
+                <Text style={{color: 'grey', fontSize:16, lineHeight: 22, marginTop: 8}}>{story?.title}</Text>
+            </View>
+            {story !== null && mapBlanks()}
+            <View style={styles.btn} >
+                <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold'}} onPress={handlePlayAgainButton}>Play Again</Text>
+            </View>
+        </View>
+        
+    </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
     header: {
